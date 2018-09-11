15be42f59afe34341faed56c39853c5e0a7c7805
yihanye <32300316+yihanye@users.noreply.github.com>
2018-09-11 10:38:34 -0400

    all key event

--- CISC474 temp/scripts/UI.js
+++ CISC474 temp/scripts/UI.js
@@ -5,16 +5,27 @@ var UI = function()
     this.initialize = function()
     {
         self.game = new cat();
-        $('#cat').css("top",self.game.xPos+'px');
-        $('#cat').css("left",self.game.yPos+'px');
+        $('#cat').css("top",self.game.yPos+'px');
+        $('#cat').css("left",self.game.xPos+'px');
+        
 
         $('body').keypress(function(event){
-            if(event.which == 32){
+            //alert( event.type + ": " +  event.which );
+            if(event.which == 119){
+                self.game.updateYpos(-50);
+            }
+            else if (event.which == 115){
+                self.game.updateYpos(50);
+            }
+            else if (event.which == 97){
                 self.game.updateXpos(-50);
-            }else{
+            }
+            else if (event.which == 100){
                 self.game.updateXpos(50);
             }
-            $('#cat').css("top",self.game.xPos+'px');
+
+            $('#cat').css("top",self.game.yPos+'px');
+            $('#cat').css("left",self.game.xPos+'px');
         });
     }
     this.initialize();
