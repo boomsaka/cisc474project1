ea501f00e9dfac9126b186e01d6773acc9f39f4f
yihanye <32300316+yihanye@users.noreply.github.com>
2018-09-13 22:24:33 -0400

    collision works

--- CISC474 temp/scripts/UI.js
+++ CISC474 temp/scripts/UI.js
@@ -1,33 +1,55 @@
 var UI = function()
 {
     var self=this;
-    this.game = undefined;
-    this.initialize = function()
+    self.game = undefined;
+    self.initialize = function()
     {
-        self.game = new cat();
-        $('#cat').css("top",self.game.yPos+'px');
-        $('#cat').css("left",self.game.xPos+'px');
-        
+        self.game = new game();
+        // update view every .1 second
+        setInterval(function(){ self.updateView()}, 100);
+        self.ketPressEvent();
+    }
 
+    //  update view 
+    self.updateView = function(){
+        $('#test').text("");
+        self.game.update();
+        $('#cat').css("top",self.game.cat.yPos+'px');
+        $('#cat').css("left",self.game.cat.xPos+'px');
+        $('#stone').css("top",self.game.stone.yPos+'px');
+        $('#stone').css("left",self.game.stone.xPos+'px');
+    }
+    // keyPressEvent
+    self.ketPressEvent = function(){
+        var cat = self.game.cat;
+        var stone = self.game.stone;
         $('body').keypress(function(event){
-            //alert( event.type + ": " +  event.which );
-            if(event.which == 119){
-                self.game.updateYpos(-50);
+            if(event.which == 119){ // "w"
+                cat.updateYpos(-50);
             }
-            else if (event.which == 115){
-                self.game.updateYpos(50);
+            else if (event.which == 115){ //"s"
+                cat.updateYpos(50);
             }
-            else if (event.which == 97){
-                self.game.updateXpos(-50);
+            else if (event.which == 97){ // "a"
+                cat.updateXpos(-50);
             }
-            else if (event.which == 100){
-                self.game.updateXpos(50);
+            else if (event.which == 100){ //"d"
+                cat.updateXpos(50);
             }
-            //alert(event.type + ": " + event.which);
-            $('#cat').css("top",self.game.yPos+'px');
-            $('#cat').css("left",self.game.xPos+'px');
+            else if (event.which == 105){ //"i"
+                stone.updateYpos(-50);
+            }
+            else if (event.which == 107){ //"k"
+                stone.updateYpos(50);
+            }
+            else if (event.which == 106){ //"j"
+                stone.updateXpos(-50);
+            }
+            else if (event.which == 108){ //"l"
+                stone.updateXpos(50);
+            }
+            //asself.updateView();
         });
     }
     this.initialize();
 }
-
--- CISC474 temp/scripts/script.js
+++ CISC474 temp/scripts/script.js
@@ -1,12 +1,32 @@
+//game class
+var game = function(){
+    this.cat = new cat();
+    this.stone = new stone();
+    this.initialize = function(){
+    }
+ 
+    this.collision = function(c,o){
+        if(Math.abs(c.xPos- o.xPos)<100 && Math.abs(c.yPos- o.yPos)<67){
+            $('#test').text("collision");
+        }
+    }  
 
+    this.update = function(time){
+        this.collision(this.cat,this.stone);
+    }
+    this.initialize();
+}
+
+// cat class and cat function
 var cat = function(){
     var self = this;
     this.xPos = 0;
     this.yPos = 0;
     this.initialize = function(){
         self.xPos = 430;
-        self.yPos = 439;
+        self.yPos = 440;
     }
+
     this.updateXpos = function(newXpos){
         var max = $('#playBoard').width();
         var cat = $('#cat').width();
@@ -26,6 +46,42 @@ var cat = function(){
             self.yPos = self.yPos + newYpos;
         } else if (newYpos > 0) {
             self.yPos = max - cat;
+        } else {
+            self.yPos = 0;
+        }
+    }
+    this.initialize();
+}
+
+var stone = function(){
+    var self = this;
+    this.xPos = 0;
+    this.yPos = 0;
+    this.initialize = function(){
+        self.xPos = 330;
+        self.yPos = 440;
+    }
+    
+    this.updateXpos = function(newXpos){
+        var max = $('#playBoard').width();
+        var cat = $('#stone').width();
+        if ((self.xPos + newXpos + cat) < max && (self.xPos + newXpos) >= 0) {
+            self.xPos = self.xPos + newXpos;
+        } else if (newXpos > 0) {
+            self.xPos = max - cat;
+        } else {
+            self.xPos = 0;
+        }
+        
+    }
+
+    this.updateYpos = function(newYpos){
+        var max = $('#playBoard').height();
+        var stone = $('#stone').height();
+        if ((self.yPos + newYpos + stone) < max && (self.yPos + newYpos) >= 0){
+            self.yPos = self.yPos + newYpos;
+        } else if (newYpos > 0) {
+            self.yPos = max - stone;
         } else {
             self.yPos = 0;
         }
