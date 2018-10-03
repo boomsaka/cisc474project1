8dcad8c4337f678a742aa1294d72962f433fb56a
Mandi Li <mandili@wifi-roaming-128-4-118-153.host.udel.edu>
2018-10-03 15:33:46 -0400

    timer updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,21 @@
-// Create a new game
-const game = new Game();
-// function myMove() {
-//   var elem = document.getElementById("playBoard");
-//   var pos = 0;
-//   var id = setInterval(frame, 20);
-//   function frame() {
-//     if (pos == 300) {
-//         myMove();
-//       clearInterval(id);
-//     } else {
-//       pos++;
-//       elem.style.right = pos + 'px';
-//     }
-//   }
-// }
+function backgroundScroller(game) {
+  var elem = document.getElementById("backgroundBoard");   
+  var pos = 0;
+  var id = setInterval(frame, 20);
+  function frame() {
+    if (pos == 300) {
+        backgroundScroller();
+        clearInterval(id);
+    } else {
+        pos++; 
+        elem.style.right = pos + 'px'; 
+    }
+  }
+}
 
+
+// Create a new game
+const game = new Game();   
+// backgroundScroller(game); 
 game.init();
-//myMove();
 const ui = new UI();
